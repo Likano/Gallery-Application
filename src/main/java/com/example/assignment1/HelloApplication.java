ea33e8e36da6c19e777c0efd7af7aebaedf54478
Likano Mbobo <mbobolikano@gmail.com>
2024-03-12 20:00:05 +0200

    1st Commit

---
+++ src/main/java/com/example/assignment1/HelloApplication.java
@@ -0,0 +1,65 @@
+package com.example.assignment1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // grid of thumbnails
+        GridPane root = new GridPane();
+
+        // An array of paths
+        String[] paths = {
+                "src/main/resources/com/example/assignment1/image1.jpeg",
+                "src/main/resources/com/example/assignment1/image2.jpeg",
+                "src/main/resources/com/example/assignment1/image3.jpeg",
+                "src/main/resources/com/example/assignment1/image4.jpeg",
+                "src/main/resources/com/example/assignment1/image5.jpeg",
+                "src/main/resources/com/example/assignment1/image6.jpeg"
+        };
+
+        // fileInputStream
+        FileInputStream[] urls = new FileInputStream[6];
+        for (int i=0; i<paths.length; i++){
+            // assigning urls to the InputFileStream
+            urls[i] = new FileInputStream(paths[i]);
+        }
+
+        // imageview array
+        ImageView[] imageview = new ImageView[6];
+        // assigning image paths
+        for (int i=0; i<paths.length; i++)
+        {
+            imageview[i] = new ImageView(paths[i]);
+            // properties of imageview
+            imageview[i].setFitWidth(160);
+            imageview[i].setFitHeight(160);
+            imageview[i].setPreserveRatio(true);
+        }
+
+        // Gridpane properties
+        root.addRow(0,imageview[0],imageview[1],imageview[2]);
+
+
+
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Gallery Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
