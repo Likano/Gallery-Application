de65cb8f3b0d9dc162494529fac63b52df6a901a
Likano Mbobo <mbobolikano@gmail.com>
2024-03-12 20:25:37 +0200

    3rd added CSS Commit

--- src/main/java/com/example/assignment1/HelloApplication.java
+++ src/main/java/com/example/assignment1/HelloApplication.java
@@ -17,6 +17,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         // grid of thumbnails
         GridPane root = new GridPane();
+        root.setHgap(6);
+        root.setVgap(11);
 
         // An array of paths
         String[] paths = {
@@ -28,14 +30,14 @@ public class HelloApplication extends Application {
                 "src/main/resources/com/example/assignment1/image6.jpeg"
         };
 
-        // fileInputStream
+        // fileInputStream dynamic way
         FileInputStream[] urls = new FileInputStream[6];
         for (int i=0; i<paths.length; i++){
             // assigning urls to the InputFileStream
             urls[i] = new FileInputStream(paths[i]);
         }
 
-        // images
+        // Loading multiple images dynamically way
         Image[] image = new Image[6];
         for (int i=0; i<urls.length; i++){
             // assigning urls to an image
@@ -43,22 +45,28 @@ public class HelloApplication extends Application {
 
         }
 
-        // imageview array
+        // displaying multiple imageview dynamically
         ImageView[] imageview = new ImageView[6];
         // assigning image paths
         for (int i=0; i<paths.length; i++)
         {
             imageview[i] = new ImageView(image[i]);
             // properties of imageview
+            imageview[i].setX(10);
+            imageview[i].setY(10);
             imageview[i].setFitWidth(160);
             imageview[i].setFitHeight(160);
             imageview[i].setPreserveRatio(true);
         }
 
-        // Gridpane properties
+        // Gridpane properties rows and columns
         root.addRow(0,imageview[0],imageview[1],imageview[2]);
+        root.addRow(1,imageview[3],imageview[4],imageview[5]);
 
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 650,400);
+
+        // linking stylesheet
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setTitle("Gallery Application");
         stage.setScene(scene);
         stage.show();
